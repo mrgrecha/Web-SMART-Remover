2336f64a06a2155a354fc4014039d898895cd337
Dima <dimon2810@tut.by>
2017-05-24 21:40:43 +0300

    Added prototypes of models. Made first migrations

--- smart_rm/models.py
+++ smart_rm/models.py
@@ -1,6 +1,33 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class Trash_bin(models.Model):
+    path_of_config = models.FilePathField
+    #TODO add other fields?
+
+
+class Task(models.Model):
+    pass
+
+class File(models.Model):
+    path = models.FilePathField
+    name = models.CharField(max_length=200)
+    type = models.CharField(max_length=50)
+    hash_of_file = models.CharField(max_length=40)
+    is_in_bin = models.BooleanField
+    size = models.BigIntegerField
+    time = models.BigIntegerField
+
+
+#             self.database = os.path.expanduser('~/.DB.json')
+#             self.max_size = 500000000
+#             self.max_number = 1000
+#             self.max_list_height = 50
+#             self.max_time = 1000
+#             self.arr_json_files = serialization.load_json(self.database)
+#             self.policies = 'default'
+#             self.silent = False
+#             self.dried = False
+#             self.interactive = False
+#             self.force = False
